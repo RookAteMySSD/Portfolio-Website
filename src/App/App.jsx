14e181c0c3a03d703936d514fc28d4a12203fa63
Ronin <rlombardino44@gmail.com>
2022-03-05 01:27:05 -0800

    added backround gradient

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from 'react'
 import {AboutMe} from './AboutMe.jsx'
 import {Projects} from './Projects.jsx'
+import {background} from './styles.jsx'
 
 export const App = () => {
   return (
-    <div>
+    <div style={background}>
       <AboutMe />
       <Projects />
     </div>
