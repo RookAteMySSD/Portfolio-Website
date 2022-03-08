9240c7aef983ed70f8f546a66836b59797c7eb3d
Ronin <rlombardino44@gmail.com>
2022-03-08 15:12:00 -0800

    added descriptions

--- src/ProjectsData.js
+++ src/ProjectsData.js
@@ -1,13 +1,13 @@
 export const ProjectsData = [
   {
     name: 'Portfolio',
-    description: 'Lorium Ipsum',
+    description: 'This is the website you\'re on right now! This website is front-end and was made with user experience in mind to concisely and effectively show my projects, experience, and skill. It has a modular framework that makes adding and removing sections easy, as well as making it easy to change links by storing them centrally.',
     tech: ['React', 'Firebase'],
     github: 'https://github.com/RookAteMySSD/Portfolio-Website'
   },
   {
     name: 'Hunt Showdown Loadout Manager',
-    description: 'Lorium Ipsum',
+    description: 'This web-app allows users to design and customize weapon and item loadouts for the online pvp shooter Hunt:Showdown. It is a full-stack application that has an easy to use and intuitive design. It allows multiple users to design loadouts without conflicting and allows users to update loadouts seamlessly.',
     tech: ['React', 'Express', 'MongoDB', 'Mongoose'],
     github: 'https://github.com/RookAteMySSD/Hunt-Showdown-Loadout-Manager'
   },
