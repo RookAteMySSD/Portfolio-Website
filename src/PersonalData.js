a419ba5773df22162731bbb1c067182db19c2f10
Ronin <rlombardino44@gmail.com>
2022-03-08 14:09:52 -0800

    added personal data folder

---
+++ src/PersonalData.js
@@ -0,0 +1,6 @@
+export const PersonalData = {
+  name: 'Ronin',
+  github: 'https://github.com/RookAteMySSD',
+  linkedin: 'https://www.linkedin.com/in/ronin-lombardino/',
+  email: 'Rlombardino44@gmail.com'
+}
