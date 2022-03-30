fee415d6bb2f2565c320c889613af2a40fefb182
Ronin <rlombardino44@gmail.com>
2022-03-30 13:52:11 -0700

    removed photo

--- src/App/AboutMe.jsx
+++ src/App/AboutMe.jsx
@@ -5,20 +5,17 @@ import './AppStyles.css'
 export const AboutMe = () => {
   return (
     <div id='aboutMe'>
-      <div style={{maxWidth: '75%'}}>
-        <h3 style={{margin: '5px'}}>I'm Ronin</h3>
-        <h4 style={{margin: '5px'}}>I am a software engineer and I build full-stack applications with a focus on good user experience and functionality</h4>
-        <p style={{margin: '5px'}}>
-          I am passionate about creating software that is easy to use, efficient, and useful to consumers.
-          <br/>I have had extensive experience working with people of all backgrounds through my volunteering, and through that have honed my communication skills as well as learning to recognise and attend to consumer needs in an efficient manner
-        </p>
-        <div>
-          <img src='./IMG/GitHub.png' className='icon' onClick={() => {location.href=`${PersonalData.github}`}}></img>
-          <img src='./IMG/LinkedIn.png' className='icon'
-          onClick={() => {location.href=`${PersonalData.linkedin}`}}></img>
-        </div>
+      <h3 style={{margin: '5px'}}>I'm Ronin</h3>
+      <h4 style={{margin: '5px'}}>I am a software engineer and I build full-stack applications with a focus on good user experience and functionality</h4>
+      <p style={{margin: '5px'}}>
+        I am passionate about creating software that is easy to use, efficient, and useful to consumers.
+        <br/>I have had extensive experience working with people of all backgrounds through my volunteering, and through that have honed my communication skills as well as learning to recognise and attend to consumer needs in an efficient manner
+      </p>
+      <div>
+        <img src='./IMG/GitHub.png' className='icon' onClick={() => {location.href=`${PersonalData.github}`}}></img>
+        <img src='./IMG/LinkedIn.png' className='icon'
+        onClick={() => {location.href=`${PersonalData.linkedin}`}}></img>
       </div>
-      <img src="./IMG/Me.jpg" id="portrait"></img>
     </div>
   )
 }
