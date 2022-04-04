5f0ca9cdddc51cd4956db56fdd2d3e46dd1a9f14
Ronin <rlombardino44@gmail.com>
2022-04-04 13:21:36 -0700

    added footer and credatiation

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,13 +1,17 @@
 import React, {useState, useEffect} from 'react'
 import {AboutMe} from './AboutMe.jsx'
 import {Projects} from './Projects.jsx'
+import {Footer} from './Footer.jsx'
 import './AppStyles.css'
 
 export const App = () => {
   return (
-    <div id='background'>
-      <AboutMe />
-      <Projects />
+    <div>
+      <div id='mainBackground'>
+        <AboutMe />
+        <Projects />
+      </div>
+      <Footer />
     </div>
   )
 }
---
+++ src/App/Footer.jsx
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from 'react'
+import './AppStyles.css'
+
+export const Footer = () => {
+ return (
+   <div id='footer'>
+     <div>Image and Icon Credit: Rosskkan</div>
+   </div>
+ )
+}
