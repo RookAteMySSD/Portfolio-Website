90d96921056a469e342f2d5eb4c1ffec22ee827f
Ronin <rlombardino44@gmail.com>
2022-04-06 14:45:08 -0700

    added project catwalk

--- src/ProjectsData.js
+++ src/ProjectsData.js
@@ -2,19 +2,25 @@ export const ProjectsData = [
   {
     name: 'Portfolio',
     description: 'This is the website you\'re on right now! This website is front-end and was made with user experience in mind to concisely and effectively show my projects, experience, and skill. It has a modular framework that makes adding and removing sections easy, as well as making it easy to change links by storing them centrally.',
-    tech: ['React', 'Firebase', 'HTML', 'CSS', 'Webpack', 'Babel'],
+    tech: ['Javascript', 'React', 'Firebase', 'HTML', 'CSS', 'Webpack', 'Babel'],
     github: 'https://github.com/RookAteMySSD/Portfolio-Website'
   },
   {
     name: 'Hunt Showdown Loadout Manager',
     description: 'This web-app allows users to design and customize weapon and item loadouts for the online pvp shooter Hunt:Showdown. It is a full-stack application that has an easy to use and intuitive design. It allows multiple users to design loadouts without conflicting and allows users to update loadouts seamlessly.',
-    tech: ['React', 'Express', 'MongoDB', 'HTML', 'CSS', 'Webpack', 'Babel'],
+    tech: ['Javascript', 'React', 'Express', 'MongoDB', 'HTML', 'CSS', 'Webpack', 'Babel'],
     github: 'https://github.com/RookAteMySSD/Hunt-Showdown-Loadout-Manager'
   },
   {
     name: 'Fun Horse Toy Boutique',
     description: 'This website is made to allow customers to buy and sell toys and collectables to other prospective users. The site is fullstack, and prioritizes user experience in its design. The site uses user authentication and login to maintain security. Additionally, this site makes use of modern routing methods to seamlessly transfer between pages with minimal load times.',
-    tech: ['React', 'Express', 'MongoDB', 'HTML', 'CSS', 'ReactRouter', 'Redux', 'Webpack', 'Babel'],
+    tech: ['Javascript', 'React', 'Express', 'MongoDB', 'HTML', 'CSS', 'ReactRouter', 'Redux', 'Webpack', 'Babel'],
     github: 'https://github.com/139BillyOcean/FunHorseToyBoutique'
+  },
+  {
+    name: 'Project Catwalk',
+    description: 'This website acts as a sale page for a clothing store. It shows an overview of the product as well as allowing the user to read or post reviews or questions, and allowing users to see and navigate to related products. It is a front-end site connecting to a backend database that was set up separately.',
+    tech: ['Javascript', 'React', 'Express', 'HTML', 'CSS', 'Webpack', 'Babel'],
+    github: 'https://github.com/Audacious-Alder/audacious-alder'
   }
 ]
