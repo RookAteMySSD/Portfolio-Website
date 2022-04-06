d490ba96a57a740c13894d43a0826c93cb18774e
Ronin <rlombardino44@gmail.com>
2022-04-06 14:39:15 -0700

    changed footer to grid

--- src/App/Footer.jsx
+++ src/App/Footer.jsx
@@ -4,6 +4,7 @@ import './AppStyles.css'
 export const Footer = () => {
  return (
    <div id='footer'>
+     <div>Created by Ronin Lombardino</div>
      <div>Image and Icon Credit: Rosskkan</div>
    </div>
  )
