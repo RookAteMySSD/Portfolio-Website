6f9cd497c3c836b17f40bda2814ecc29580127c1
Ronin <rlombardino44@gmail.com>
2022-04-07 22:28:01 -0700

    added email link

--- src/App/AboutMe.jsx
+++ src/App/AboutMe.jsx
@@ -12,6 +12,8 @@ export const AboutMe = () => {
         <br/>I have had extensive experience working with people of all backgrounds through my volunteering, and through that have honed my communication skills as well as learning to recognise and attend to consumer needs in an efficient manner
       </p>
       <div id='iconBox'>
+      <img src='./IMG/Gmail.png' className='icon' id='Gmail'
+        onClick={() => {location.href=`mailto:${PersonalData.email}`}}></img>
         <img src='./IMG/GitHub.png' className='icon' id='GitHub'
         onClick={() => {location.href=`${PersonalData.github}`}}></img>
         <img src='./IMG/LinkedIn.png' className='icon' id='Linkedin'
