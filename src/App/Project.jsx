5685e5fce95b3f5e855e84c6f500290790c88e4d
Ronin <rlombardino44@gmail.com>
2022-04-08 23:34:15 -0700

    changed from default link for projects

--- src/App/Project.jsx
+++ src/App/Project.jsx
@@ -4,7 +4,7 @@ import './AppStyles.css'
 export const Project = ({project}) => {
   return (
     <div className='projectBox'>
-    <h4 className='projectTitle'><a href='https://github.com/RookAteMySSD/Hunt-Showdown-Loadout-Manager' target="_blank">{project.name}</a></h4>
+    <h4 className='projectTitle'><a href={project.github} target="_blank">{project.name}</a></h4>
     <div className='sectionBox'>{project.description}</div>
     <div className='techHolder'>{project.tech.map((tech) => <div className='techBox'>{tech}</div>)}</div>
     </div>
